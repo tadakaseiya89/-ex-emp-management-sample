2d7b43f3f25ef09463fdd54d0a1148112224c30a
tadakaseiya <seiya.tadaka@rakus-partners.co.jp>
2022-02-07 13:59:11 +0900

    フォーム作成

--- src/main/java/ex/sample/form/InsertAdministratorForm.java
+++ src/main/java/ex/sample/form/InsertAdministratorForm.java
@@ -1,5 +1,39 @@
 package ex.sample.form;
 
+/**
+ * @author 田高誠也
+ *
+ */
 public class InsertAdministratorForm {
+	/** 名前 */
+	private String name;
+	/** メールアドレス */
+	private String mailAddress;
+	/** パスワード */
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMailAddress() {
+		return mailAddress;
+	}
+
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
--- src/main/java/ex/sample/service/AdministratorService.java
+++ src/main/java/ex/sample/service/AdministratorService.java
@@ -1,5 +1,17 @@
 package ex.sample.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ex.sample.repository.AdministratorRepository;
+
+@Service
+@Transactional
 public class AdministratorService {
+@Autowired
+	private AdministratorRepository administratorRepository;
+
+
 
 }
